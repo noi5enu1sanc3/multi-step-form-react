b996472bfbff9cea50f825cefd96c3e03a19b6f0
noisenuisance <sabauspne@gmail.com>
2023-02-18 19:05:51 +0300

    add scss spacing & fonts variables

--- src/components/StepsBar/StepsBar.jsx
+++ src/components/StepsBar/StepsBar.jsx
@@ -23,21 +23,21 @@ function StepsBar({ currentStep }) {
   ];
 
   return (
-    <nav className="steps-bar-container">
+    <aside className="steps-bar-container">
       <ul className="steps-bar-list">
         {
-          stepsData.map((item) => (
-            <li className="bar-item" key={item.step}>
-              <button type="button" className="bar-item__button">
-                <span className={`bar-item__step-number ${item.step === currentStep ? 'bar-item__step-number_active' : ''}`}>{item.step}</span>
-                <span className="bar-item__step-title">{`STEP ${item.step}`}</span>
-                <span className="bar-item__step-subtitle">{item.subtitle}</span>
-              </button>
-            </li>
-          ))
-        }
+            stepsData.map((item) => (
+              <li className="bar-item" key={item.step}>
+                <button type="button" className="bar-item__button">
+                  <span className={`bar-item__step-number ${item.step === currentStep ? 'bar-item__step-number_active' : ''}`}>{item.step}</span>
+                  <span className="bar-item__step-title">{`STEP ${item.step}`}</span>
+                  <span className="bar-item__step-subtitle">{item.subtitle}</span>
+                </button>
+              </li>
+            ))
+          }
       </ul>
-    </nav>
+    </aside>
   );
 }
 
