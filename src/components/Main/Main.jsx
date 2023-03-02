a9f8e6f93c0560a3f1fdad7ebebb0f7fe1098e99
noisenuisance <sabauspne@gmail.com>
2023-03-02 21:28:22 +0300

    fix: use step number constant in Main

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,10 +2,11 @@ import { useSelector } from 'react-redux';
 import './Main.scss';
 import StepsBar from '../StepsBar/StepsBar';
 import FormContainer from '../FormContainer/FormContainer';
+import { RESULT_SECTION_STEP_NUMBER } from '../../utils/constants';
 
 function Main() {
   const currentStep = useSelector((state) => state.formUpdater.step);
-  const isStepsBarVisible = currentStep !== 5;
+  const isStepsBarVisible = currentStep !== RESULT_SECTION_STEP_NUMBER;
 
   return (
     <main className="main">
